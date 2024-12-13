f591989ee8cf54c1205df5ff655f0be10b2eb32b
Mustafa Redifoglu <mustafaredifoglu@gmail.com>
2024-12-13 14:40:26 +0300

    all tests passed

--- src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -3,38 +3,40 @@ package com.workintech.fswebs17d1.controller;
 import com.workintech.fswebs17d1.entity.Animal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/workintech")
 public class AnimalController {
-    private Map<Integer, Animal> animals;
 
-    public AnimalController(Map<Integer, Animal> animals) {
-        this.animals = animals;
-    }
+    private  Map<Integer, Animal> animals = new HashMap<>();
 
     @GetMapping("/animal")
-    public List<Animal> getAllAnimals(){
-        return new ArrayList<>(animals.values());
+    public List<Animal> getAnimals() {
+        return animals.values().stream().toList();
     }
     @GetMapping("/animal/{id}")
-    public Animal getAnimalsById(@PathVariable int id){
+    public Animal getAnimal(@PathVariable int id) {
         return animals.get(id);
     }
+
     @PostMapping("/animal")
-    public void addAnimal(@RequestBody Animal animal){
-        animals.put(animal.getId(),animal);
-    }
-    @PutMapping("/animal/{id}")
-    public void updateAnimal(@PathVariable int id,@RequestBody Animal animal){
-        animals.put(id,animal);
-    }
-    @DeleteMapping("/animal/{id}")
-    public void deleteAnimal(@PathVariable int id){
-        animals.remove(id);
+    public Animal setAnimals(@RequestBody Animal animal) {
+        animals.put(animal.getId(), animal);
+        return animal;
     }
 
+    @PutMapping("/animal/{id}")
+    public Animal updateAnimals(@PathVariable int id, @RequestBody Animal animal) {
+        animal.setId(id);
+        animals.put(animal.getId(), animal);
+        return animal;
+    }
+
+    @DeleteMapping("/animal/{id}")
+    public void deleteAnimals(@PathVariable int id) {
+        animals.remove(id);
+    }
 }
--- src/main/java/com/workintech/fswebs17d1/entity/Animal.java
+++ src/main/java/com/workintech/fswebs17d1/entity/Animal.java
@@ -1,38 +1,31 @@
 package com.workintech.fswebs17d1.entity;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Animal {
-    private int id;
+    private Integer id;
     private String name;
 
-    public Animal(int id, String name) {
+    public Animal() {
+    }
+
+    public Animal(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
+    public Animal setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Animal{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return null;
     }
 }
